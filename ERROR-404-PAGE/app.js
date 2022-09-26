a678166441e73abd39874978ebfa58231e715bf4
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-09-26 18:47:25 -0500

    termino la pagina de error 404 y actualizo el .readme

--- ERROR-404-PAGE/app.js
+++ ERROR-404-PAGE/app.js
@@ -1,34 +1,35 @@
 // obtencióon de elementos html
-
+const main_cont = document.getElementById("cont") ;
 const content_req = document.getElementById("cont-req");
 const btn_snd = document.getElementById("snd");
-
 
 
 // funciones a ejecutar.
 
  function cleanHTML(){
+// elimina el html del inicio que ejecuta la petición
     content_req.remove();
 };
 
 function errorPage (){
+
     const htmlCode = `<div class="cont">
     <img src="img/404 error with a landscape-bro.svg" alt="">
-        <p>The page or information you are looking for has not been found.</p>
+    <p>The page or information you are looking for has not been found.</p>
         <a href="">contact technical support team and customer service.</a>
-        <button>Back to top</button>
+            <button>Back to top</button>
     </div>`;
-    
-   const  newDiv = document.createElement("div");
 
-   newDiv.innerHTML = htmlCode;
+    main_cont.innerHTML =  htmlCode;
+
 
 }
 
 const req = function (){
     
+    // genero una petición erronea a una API
     const key = `db2579c0c8eafb1dda8df201dad33d93`;
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=bogotaa,colombia,&appid=${key}`;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=bogot,colombia,&appid=${key}`;
 
 
     fetch(url)
@@ -36,13 +37,16 @@ const req = function (){
         .then((dataResolve)=>{
             if (dataResolve.cod === "404") {
                 cleanHTML();
+                errorPage();
+
             }
         })
 }
 
 
-// ejecto la funcion
+// ejecto la funcion de la pettición.
 
 btn_snd.addEventListener("click",req);
 
 
+// 26/09/2022
