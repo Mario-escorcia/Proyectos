bc6c673ca2577885a12d072cdaebda9b1d4576f1
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-11-08 11:32:34 -0500

    termino el navegador (clon de google)

--- BROWSER/app.js
+++ BROWSER/app.js
@@ -1,1 +1,30 @@
-// const url = 'https://www.google.com/search?q='
+
+
+const searchIcon = document.getElementById("search--icon");
+const query = document.getElementById("query");
+const searchBtn = document.getElementById("search--btn");
+const doodleBtn = document.getElementById("doodle--btn");
+
+// funcion principal para ejecutar la busqueda
+function search() {
+    let url = 'https://www.google.com/search?q='+query.value;
+    window.open(url,"_self"); 
+}
+// funcion para ir a doodles
+function Lucky() {
+    let  doodle = 'https://www.google.com/doodles';
+    window.open(doodle,"_self"); 
+}
+
+// funcion para ejecutar la busqueda si se presiona la tecla enter
+document.addEventListener("keypress",(e)=>{
+    if (e.keyCode == 13) {
+        search();
+    }
+
+})
+
+// ejecuto los eventListeners
+searchIcon.addEventListener("click" , search);
+searchBtn.addEventListener("click",search)
+doodleBtn.addEventListener("click" , Lucky);
