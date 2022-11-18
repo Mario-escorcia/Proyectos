45b8fa9dd08c5f255b25ae94d9ecc7253e5ace01
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-11-18 11:31:56 -0500

    ya se realiza correctamente la petición a la api , falta mostrar en pantalla

--- weather--React/src/components/ComponentePrincipal/Principal.jsx
+++ weather--React/src/components/ComponentePrincipal/Principal.jsx
@@ -1,5 +1,31 @@
 import './Principal.css'
+// import {useState , useEffect} from 'react'
+
+
 function Principal() {
+    function peticion() {
+            
+        // setCiudad(inputCiudad.value)
+        // setPais(inputPais.value)
+        const ciudad = document.querySelector(".nombre_ciudad");
+        const pais = document.getElementById("pais");
+        const key = `db2579c0c8eafb1dda8df201dad33d93`;
+        const url =`https://api.openweathermap.org/data/2.5/weather?q=${ciudad.value},${pais.value}&appid=${key}`;
+
+       try {
+        fetch(url)
+        .then((api)=>{ return api.json()})
+        .then((apiData)=>
+        { return console.log(apiData)})
+       } catch (error) {
+        console.log(error)
+       }
+
+    }
+
+        // const [ciudad, setCiudad] = useState("");
+        // const [pais, setPais] = useState("");
+
   return (
 
         <div className="principal__cont">
@@ -19,10 +45,10 @@ function Principal() {
 
             <div className="inputs__cont">
 
-                <input className='nombre_ciudad' autoComplete='off' autoCapitalize='off' type="text" placeholder='Escriba una ciudad aquí' />
+                <input className='nombre_ciudad'type="text" placeholder='Escriba una ciudad aquí' />
 
                 <select name="" id="pais" >
-                    <option class="opt-pais" disabled selected value="">seleccione un pais</option>
+                    <option className='opt-pais' disabled selected value="">seleccione un pais</option>
                     <option value="CO">colombia</option>
                     <option value="BR">Brasil</option>
                     <option value="MX">Mexico</option>
@@ -33,7 +59,7 @@ function Principal() {
                     <option value="JPN">japon</option>
                 </select>
 
-                <button>Ver Clima</button>
+                <button onClick={peticion}>Ver Clima</button>
             </div>
         </div>
 
