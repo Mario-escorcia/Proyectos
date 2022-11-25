82bd58b6a356c4562505952d93bf5aa01420c817
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-11-25 09:52:36 -0500

    codigo del reloj a esta inicial

--- reloj/app.js
+++ reloj/app.js
@@ -10,7 +10,7 @@ setInterval(()=>{
         let DD = date.getDay();
         let MM = date.getMonth();
         let AA = date.getFullYear();
-    
+
         hh = hh < 10 ? `0${hh}` : hh;
         mm = mm < 10 ? `0${mm}` : mm;
         ss = ss < 10 ? `0${ss}` : ss;
@@ -18,11 +18,10 @@ setInterval(()=>{
         DD = DD < 10 ? `0${DD}` : DD;
         MM = MM < 10 ? `0${MM}` : MM;
         AA = AA < 10 ? `0${AA}` : AA;        
-        
+
 
         let hourResult = `${hh}:${mm}:${ss}`;
         let dateResult = `${DD}/${MM}/${AA}`;
         hora.innerText = hourResult;
         fecha.innerText = dateResult;
     },1000)
-
